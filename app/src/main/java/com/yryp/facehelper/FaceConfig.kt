2bd830dce3c0c065e1f330823b61ce825165c0cc
xiantm <60340960@qq.com>
2018-07-19 14:27:17 +0800

    测试速度

--- app/src/main/java/com/yryp/facehelper/FaceConfig.kt
+++ app/src/main/java/com/yryp/facehelper/FaceConfig.kt
@@ -1,12 +1,12 @@
 package com.yryp.facehelper
 
 object FaceConfig {
-    val appid = "DPHDysP1UTq3zUVDDQnPpFXPB9S7pj4mR9eXQz71Rt49"
-    val ft_key = "4wo2BKhUpE2npBnpRqbxsoXibASHeThZ96geMd2ENuFh"
-    val fd_key = "4wo2BKhUpE2npBnpRqbxsoXqkZhUZJ3Suj7E5qEujzBC"
-    val fr_key = "4wo2BKhUpE2npBnpRqbxsoXxuxxbdUeszmtvfrk3maQa"
-    val age_key = "4wo2BKhUpE2npBnpRqbxsoYaiyGUxthqZjytsXs6gnoq"
-    val gender_key = "4wo2BKhUpE2npBnpRqbxsoYhtNXdmz5V1emmHNpk7Lvs"
+    val appid =
+    val ft_key =
+    val fd_key =
+    val fr_key =
+    val age_key =
+    val gender_key =
     var previewHeight = 480
     var previewWidth = 640
     var rotate = 270
